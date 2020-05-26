7dd88a4d1640a966dea53231582fb9791bba53f1
berfinson <berfinsonmeez@hotmail.com>
2020-05-26 18:36:17 +0300

    iki method silindi

--- mobilproje/app/src/main/java/com/example/mobilproje/anamenuActivity.java
+++ mobilproje/app/src/main/java/com/example/mobilproje/anamenuActivity.java
@@ -2,7 +2,6 @@ package com.example.mobilproje;
 
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,12 +13,5 @@ public class anamenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anamenu);
     }
-    public void onclickGoToIlaclar(View view) {
-        setContentView(R.layout.activity_ilacmenusu);
 
-    }
-    public void onclickGoToNotlar(View view) {
-        setContentView(R.layout.activity_notlar);
-
-    }
 }
